a18efb0a9fb0fb71337f150a904072fa0813757a
basscee1604 <basscee1604@gmail.com>
2026-02-15 00:18:45 +0000

    Update accounts models for wallet connectivity

--- Findxo/django_api/accounts/models.py
+++ Findxo/django_api/accounts/models.py
@@ -2,7 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class SupabaseUser(models.Model):
+    id = models.UUIDField(primary_key=True)
+    wallet_address = models.CharField(max_length=128, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    wallet_connected_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'users'
+        managed = False
+        app_label = 'accounts'
+
+    def __str__(self):
+        return f"SupabaseUser({self.wallet_address})"
+
 class UserProfile(models.Model):
+
 	"""
 	Extension of Django's built-in User with an optional unique wallet address.
 	Users can authenticate using their username/email or this wallet address
